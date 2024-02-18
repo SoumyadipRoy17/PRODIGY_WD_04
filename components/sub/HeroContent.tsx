f0a07288cab4abe2db9e8a38549e1731aa70a391
Soumyadip Roy <soumyaroy172003@gmail.com>
2024-02-18 23:44:01 +0530

    changed the  starter text

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -31,12 +31,17 @@ export default function HeroContent() {
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
-          Providing
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            the best
-          </span>
+          Hey, I am{" "}
           <motion.div variants={slideInFromRight(0.8)}>
-            Project expierience
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              Soumyadip Roy
+            </span>
+          </motion.div>
+          <motion.div
+            variants={slideInFromTop}
+            className="flex flex-col gap-6 mt-1 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+          >
+            A Full Stack Developer
           </motion.div>
         </motion.div>
         <motion.p
